8d61f6b24791e05f98ae29c2e97a295f77b066b6
TBWA-LAMATRIX <nicolas.ludovic@gmail.com>
2014-11-06 12:28:04 +0400

    Initialisation

---
+++ src/Ifendirh/SyncBundle/Message/SyncMessage.php
@@ -0,0 +1,7 @@
+<?php
+namespace Ifendirh\SyncBundle\Message;
+
+class SyncMessage{
+	const DIRECTORY_TO_SYNC = 'Synchroniser le répertoire /var/www';
+	const DIRECTORY_NOT_EXIST = 'Le répertoire n\'existe pas';
+}
