687610de1be70d144fdc6761b5a251efb138eedd
TBWA-LAMATRIX <nicolas.ludovic@gmail.com>
2014-11-06 14:04:59 +0400

    fixed untracked nbproject

--- src/Ifendirh/SyncBundle/Message/SyncMessage.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Ifendirh\SyncBundle\Message;
-
-class SyncMessage{
-	const DIRECTORY_TO_SYNC = 'Synchroniser le répertoire /var/www';
-	const DIRECTORY_NOT_EXIST = 'Le répertoire n\'existe pas';
-}
